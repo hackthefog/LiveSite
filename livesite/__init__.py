5d6be3e5775791e31cb98668d1eb75d8e6ab5b08
Savagecoder77 <savagecoder77@gmail.com>
2019-09-09 11:59:24 -0700

    initialze database

--- livesite/__init__.py
+++ livesite/__init__.py
@@ -1,11 +1,19 @@
 # Flask imports
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 # Create Flask app
 app = Flask(__name__)
 
+# Create DB
+db = SQLAlchemy(app)
+
 # Add Configurations to app
 app.config.from_pyfile('config.py', silent=True)
+
+# importing all the models and initializing them
+from livesite.models import *
+db.create_all()
 
 # Import all views
 import livesite.views

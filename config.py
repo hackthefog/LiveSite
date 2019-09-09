cefeca3664dfae5f89e424dc2195269222985072
Savagecoder77 <savagecoder77@gmail.com>
2019-09-09 11:51:17 -0700

    new config for database

--- config.py
+++ config.py
@@ -3,3 +3,7 @@ import os
 
 # random for testing
 SECRET_KEY = os.urandom(32)
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+	        			  'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_COMMIT_ON_TEARDOWN = True

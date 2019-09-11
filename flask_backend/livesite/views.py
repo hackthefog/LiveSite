d440fa8f01e792b2b99ae1b376377f5a6b3a2112
Savagecoder77 <savagecoder77@gmail.com>
2019-09-11 09:58:29 -0700

    chaging flask app to api backend

--- flask_backend/livesite/views.py
+++ flask_backend/livesite/views.py
@@ -6,11 +6,7 @@ from flask import render_template, redirect, url_for
 def index():
     return render_template('flask_index.html')
 
-@app.route("/404", methods=['GET'])
-def error_404():
-    return render_template('404.html'), 404
-
 # Error Handelers
 @app.errorhandler(404)
 def page_not_found(e):
-    return redirect(url_for('error_404'))
+    return 404

90b34f5f160860fba8048876d2b8aa08e0359a53
Savagecoder77 <savagecoder77@gmail.com>
2019-09-12 22:16:43 -0700

    add pusher code when go to home route for testing

--- flask_backend/livesite/views.py
+++ flask_backend/livesite/views.py
@@ -1,9 +1,20 @@
 from livesite import app
 from flask import render_template, redirect, url_for
+import pusher
+from os import environ
 
 # Views
 @app.route("/", methods=['GET'])
 def index():
+	  channels_client = pusher.Pusher(
+  	  	app_id=environ['PUSHER_ID'],
+    		key=environ['PUSHER_KEY'],
+    		secret=environ['PUSHER_SECRET'],
+  		  cluster=environ['PUSHER_CLUSTER'],
+  	  	ssl=True
+	  )
+
+	  channels_client.trigger('my-channel', 'my-event', {'message': 'hello world'})
     return 200
 
 # Error Handelers

394570604a7f892df8c12bd80b292f056c4cc11e
Savagecoder77 <savagecoder77@gmail.com>
2019-11-16 22:17:47 -0800

    clean up

--- config.py
+++ config.py
@@ -3,11 +3,3 @@ from os import urandom
 
 # random for testing
 SECRET_KEY = urandom(32)
-
-firebase_config = {
-  "apiKey": "apiKey",
-  "authDomain": "projectId.firebaseapp.com",
-  "databaseURL": "https://databaseName.firebaseio.com",
-  "storageBucket": "projectId.appspot.com",
-  "serviceAccount": "path/to/serviceAccountCredentials.json"
-}

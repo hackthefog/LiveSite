30cadeaaadc668fd9d083492aa388e2028a99b2b
Rafael Cenzano <32753063+RafaelCenzano@users.noreply.github.com>
2019-11-16 21:30:57 -0800

    clean up remove pusher

--- livesite/views.py
+++ livesite/views.py
@@ -1,26 +1,11 @@
 from livesite import app
 from flask import render_template, redirect, url_for
-import pusher
-from os import environ
 
 
 # Views
 @app.route("/", methods=['GET'])
 def index():
     return render_template('home.html')
-
-@app.route("/test", methods=['GET'])
-def test():
-    channels_client = pusher.Pusher(
-        app_id=environ['PUSHER_ID'],
-        key=environ['PUSHER_KEY'],
-        secret=environ['PUSHER_SECRET'],
-        cluster=environ['PUSHER_CLUSTER'],
-        ssl=True
-    )
-    
-    channels_client.trigger('my-channel', 'my-event', {'message': 'hello world'})
-    return 'Hello World'
 
 # Error Handelers
 @app.errorhandler(404)

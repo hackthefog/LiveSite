3897a13c4671ed7ac45a3f8ea6f6533d8b72e16c
Savagecoder77 <savagecoder77@gmail.com>
2019-11-16 22:17:52 -0800

    remove

--- livesite/__init__.py
+++ livesite/__init__.py
@@ -1,6 +1,5 @@
 # Flask imports
 from flask import Flask
-import pyrebase
 import config
 
 # Create Flask app
@@ -8,9 +7,6 @@ app = Flask(__name__)
 
 # Add Configurations to app
 app.config.from_pyfile('config.py', silent=True)
-
-# Create firebase object
-firebase = pyrebase.initialize_app(config.firebase_config)
 
 # Import all views
 import livesite.views

22e5c42fee9256fbb99f5cc916468c928860d81f
Savagecoder77 <savagecoder77@gmail.com>
2019-12-17 12:15:55 -0800

    remove puzzle

--- livesite/views.py
+++ livesite/views.py
@@ -11,12 +11,6 @@ from flask import render_template, redirect, url_for
 def index():
     return render_template('home.html')
 
-
-@app.route("/puzzle", methods=['GET'])
-@app.route("/puzzle/", methods=['GET'])
-def puzzle():
-    return render_template('puzzle.html')
-
 # Error Handelers
 @app.errorhandler(404)
 def page_not_found(e):

6b6190fee2d1cdda346f4e425888a05db5ad3927
calee14 <caplee1000@gmail.com>
2019-12-19 22:26:41 -0800

    adding the admin route for db changes

--- livesite/views.py
+++ livesite/views.py
@@ -1,6 +1,6 @@
 from livesite import app
-from flask import render_template, redirect, url_for
-
+from flask import render_template, redirect, url_for, request
+from livesite.firebaseauth import *
 
 # Views
 @app.route("/", methods=['GET'])
@@ -11,3 +11,26 @@ def index():
 @app.errorhandler(404)
 def page_not_found(e):
     return 404
+
+@app.route("/admin", methods=['GET', 'POST'])
+def admin():
+	'''
+	For admin login
+	POST: 
+		form['email']: String
+		form['password']: String
+	GET:
+		Parameters: null
+	'''
+	if request.method == 'POST':
+		# check if form is empty
+		if request.form['email'] == None and request.form['password'] == None:
+			# send back to login
+			return render_template('admin.html')
+		else:
+			user = get_user_by_email(request.form['email'])
+			# authenticated = is_user_authenticated(user, request.form['password'])
+			return user.uid
+	else:
+		# send to login
+		return render_template('admin.html')

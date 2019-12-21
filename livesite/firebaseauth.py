6fbce87fcddf302116aa8445cea9a36a4f1ecf29
calee14 <caplee1000@gmail.com>
2019-12-20 18:39:16 -0800

    changing the access of certain initializations

--- livesite/firebaseauth.py
+++ livesite/firebaseauth.py
@@ -5,13 +5,16 @@ from firebase_admin import auth
 from firebase_admin import exceptions
 from firebase_admin import db
 
+from livesite.confidential.secret_data import db_url
+
 def initialize_admin_credientials():
 	import os
 	path = os.path.dirname(os.path.abspath(__file__))
 	cred = credentials.Certificate(path + "/confidential/htf-live-firebase-adminsdk-ilojf-8e3541b3a1.json")
 	fir_app = firebase_admin.initialize_app(cred, {
-		'databaseURL': 'https://databaseName.firebaseio.com'
+		'databaseURL': db_url
 		})
+	return fir_app
 	# firebase_admin.delete_app(fir_app)
 
 def initialize_basic_credientials():
@@ -19,14 +22,11 @@ def initialize_basic_credientials():
 	path = os.path.dirname(os.path.abspath(__file__))
 	cred = credentials.Certificate(path + "/confidential/htf-live-firebase-adminsdk-ilojf-8e3541b3a1.json")
 	fir_app = firebase_admin.initialize_app(cred, {
-		'databaseURL': 'https://databaseName.firebaseio.com',
+		'databaseURL': db_url,
 		'databaseAuthVariableOverride': {
 	    	'uid': 'my-service-worker'
 	    }
-	})
+	}, name='basic')
+	return fir_app
 	# firebase_admin.delete_app(fir_app)
-    
-def get_database_ref():
-	ref = db.reference()
-	print(db.uid)
-	print(ref.get())
+   

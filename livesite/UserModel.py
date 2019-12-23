cd0749ee96892ed4c2be68532f58664deeceed21
calee14 <caplee1000@gmail.com>
2019-12-23 11:39:54 -0800

    adding a User model for admin login

---
+++ livesite/UserModel.py
@@ -0,0 +1,14 @@
+import hashlib
+from flask_login import UserMixin
+
+class User(UserMixin):
+
+    def __init__(self, email, password, id=None):
+        self.id = id or hashlib.md5(str(email).encode('utf-8')).hexdigest()
+        self.email = email
+        self.password = password
+    
+    def get_id(self):
+    	return self.id
+    def __repr__(self):
+        return "%d/%s" % (self.id, self.email)

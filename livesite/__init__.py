70b8ef81c87784d9e7bc657abe49d67e9c1da20f
calee14 <caplee1000@gmail.com>
2019-12-23 11:37:17 -0800

    accessing the config

--- livesite/__init__.py
+++ livesite/__init__.py
@@ -1,5 +1,6 @@
 # App imports
 from flask import Flask
+from flask_login import LoginManager
 import config
 import firebase_admin
 from firebase_admin import credentials
@@ -11,8 +12,12 @@ fir_admin = initialize_admin_credientials()
 # Create Flask app
 app = Flask(__name__)
 
+# Create login manager for admin
+login_manager = LoginManager()
+login_manager.init_app(app)
+
 # Add Configurations to app
-app.config.from_pyfile('config.py', silent=True)
+app.config.from_pyfile('../config.py', silent=True)
 
 # Import all views
 import livesite.views

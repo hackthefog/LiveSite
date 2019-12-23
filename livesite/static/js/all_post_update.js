717f0ef0dee150f9de9e368877cc32a4adbde7d6
calee14 <caplee1000@gmail.com>
2019-12-23 10:23:01 -0800

    adding the all post loader

---
+++ livesite/static/js/all_post_update.js
@@ -0,0 +1,19 @@
+function update_all_post() {
+	$.ajax({
+		url: "/update_all_posts",
+		type: "get",
+		data: null,
+		success: function(response) {
+			$("#location_all_post").html(response);
+		},
+		error: function(xhr) {
+			//Do Something to handle error
+		}
+	});
+}
+
+update_all_post()
+var timer, delay = 500; // 500 milliseconds = half a second
+timer = setInterval(function(){
+    update_post()
+}, delay);

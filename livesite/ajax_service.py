b69d98fb8e5195fdfc6240af618a485c70fdfa84
calee14 <caplee1000@gmail.com>
2019-12-22 16:44:31 -0800

    claiming as useless code

--- livesite/ajax_service.py
+++ livesite/ajax_service.py
@@ -1,7 +1,19 @@
 from livesite import app
 from flask import render_template, redirect, url_for
 
-def update_template_post(template, recent_post=None):
-	# with app.app_context():
-	# 	redirect(url_for('update_recent_posts'), recent_post=post)
-	raise StatusDenied
+recent_post = None
+
+class StatusDenied(Exception):
+    pass
+
+@app.errorhandler(StatusDenied)
+def redirect_on_status_denied(error):
+    print("hello world")
+    return redirect(url_for('update_recent_posts', recent_post=recent_post))
+
+def update_template_post(post=None):
+    # with app.app_context():
+    # 	redirect(url_for('update_recent_posts'), recent_post=post)
+    global recent_post
+    recent_post = post
+    raise StatusDenied

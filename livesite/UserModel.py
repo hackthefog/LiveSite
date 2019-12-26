e725d482534ee04977963bff4e06c97b70a1c345
Savagecoder77 <savagecoder77@gmail.com>
2019-12-26 10:09:21 -0800

    change import and pep 8

--- livesite/UserModel.py
+++ livesite/UserModel.py
@@ -1,14 +1,16 @@
-import hashlib
+from hashlib import md5
 from flask_login import UserMixin
+
 
 class User(UserMixin):
 
     def __init__(self, email, password, id=None):
-        self.id = id or hashlib.md5(str(email).encode('utf-8')).hexdigest()
+        self.id = id or md5(str(email).encode('utf-8')).hexdigest()
         self.email = email
         self.password = password
-    
+
     def get_id(self):
-    	return self.id
+        return self.id
+
     def __repr__(self):
         return "%d/%s" % (self.id, self.email)

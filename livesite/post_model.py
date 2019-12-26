3f623cb02eb05c8becef511160e8b202429bf04c
Savagecoder77 <savagecoder77@gmail.com>
2019-12-26 10:10:01 -0800

    pep 8 standard for python files

--- livesite/post_model.py
+++ livesite/post_model.py
@@ -2,31 +2,36 @@ from firebase_admin import auth
 import hashlib
 import time
 
+
 def create_post(title, content, time, imageurl=None):
-	'''
-	Parameters:
-		Title: String
-		Content: String
-		Time: Date object
-		Imageurl: String
-	Returns:
-		JSON: dictionary of post
-	'''
-	post_id = hashlib.md5(str(time).encode('utf-8')).hexdigest()
-	post = {
-		post_id: {
-			"title": title,
-			"content": content,
-			"time": time,
-			"imageurl": imageurl
-		}
-	}
-	return post
+    '''
+    Parameters:
+            Title: String
+            Content: String
+            Time: Date object
+            Imageurl: String
+    Returns:
+            JSON: dictionary of post
+    '''
+    post_id = hashlib.md5(str(time).encode('utf-8')).hexdigest()
+    post = {
+        post_id: {
+            "title": title,
+            "content": content,
+            "time": time,
+            "imageurl": imageurl
+        }
+    }
+    return post
+
 
 class Post(object):
-	def __init__(self, data):
-		self.title = data.get('title')
-		self.content = data.get('content')
-		self.time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(data.get('time')))
-		self.float_time = data.get('time')
-		self.imageurl = data.get('imageurl') # may be null
+    def __init__(self, data):
+        self.title = data.get('title')
+        self.content = data.get('content')
+        self.time = time.strftime(
+            '%Y-%m-%d %H:%M:%S',
+            time.localtime(
+                data.get('time')))
+        self.float_time = data.get('time')
+        self.imageurl = data.get('imageurl')  # may be null
